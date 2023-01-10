95b4a9b72d075fdd46d2c1a8992851cf7407b0a0
kashifmehmood9 <econ.mehmoodkashif@gmail.com>
2023-01-10 16:38:34 +0800

    remove boiler plate documentations

--- app/src/test/java/com/example/agecalculator/ExampleUnitTest.kt
+++ app/src/test/java/com/example/agecalculator/ExampleUnitTest.kt
@@ -1,11 +1,5 @@
 package com.example.agecalculator
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
-
 import org.junit.Assert.*
 import org.junit.Test
 
